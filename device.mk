4d1b2d892c82a9c6bcf9fd7940f0ff5ce7f0dcc8
GeoPD <geoemmanuelpd2001@gmail.com>
2020-11-11 11:48:34 +0530

    sakura: Include GcamGo
    Credits: https://www.celsoazevedo.com/files/android/google-camera/f/changelog1447/
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-off-by: Zabuka <yusufnoor786@gmail.com>

--- device.mk
+++ device.mk
@@ -137,16 +137,20 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.cameraservice.common@2.0 \
-    android.frameworks.cameraservice.device@2.0 \
-    android.frameworks.cameraservice.service@2.0 \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
+    android.hardware.camera.device@3.4 \
+    vendor.qti.hardware.camera.device@1.0.so \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.qti.hardware.camera.postproc@1.0 \
-    libdng_sdk.vendor 
+    libdng_sdk.vendor \
+    libgui_vendor \
+    CameraGo
+
+# Component overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # Configstore
 PRODUCT_PACKAGES += \
